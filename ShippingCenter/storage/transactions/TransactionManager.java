c9fff14b6b35855531ad49bbf3c0920915e070d7
Cullen Sturdivant <potatophusion@gmail.com>
2017-10-02 22:13:27 -0500

    Transaction Manager complete.

---
+++ ShippingCenter/storage/transactions/TransactionManager.java
@@ -0,0 +1,48 @@
+package storage.transactions;
+
+import java.util.Date;
+
+/**
+ * Handles package transaction details.
+ *
+ * @author Cullen Sturdivant
+ * @version 1.0, 10/2/2017
+ */
+public class TransactionManager {
+    
+    private int customerID;
+    private String trackingNumber;
+    private Date shippingDate;
+    private Date deliveryDate;
+    private float shippingCost;
+    private int employeeID;
+    
+    public TransactionManager(int customerID, String trackingNumber, Date shippingDate,
+                              Date deliveryDate, float shippingCost, int employeeID) {
+        this.customerID = customerID;
+    }
+    
+    public int getCustomerID() {
+        return this.customerID;
+    }
+    
+    public String getTrackingNumber() {
+        return this.trackingNumber;
+    }
+    
+    public Date getShippingDate() {
+        return this.shippingDate;
+    }
+    
+    public Date getDeliveryDate() {
+        return this.deliveryDate;
+    }
+    
+    public float getShippingCost() {
+        return this.shippingCost;
+    }
+    
+    public int employeeID() {
+        return this.employeeID;
+    }
+}
