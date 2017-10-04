c2c61485f32a91e39b3a19748e92615f2abda054
Cullen Sturdivant <potatophusion@gmail.com>
2017-10-03 22:53:37 -0500

    More javadocs complete

--- ShippingCenter/storage/users/Employee.java
+++ ShippingCenter/storage/users/Employee.java
@@ -1,5 +1,6 @@
 package storage.users;
 
+import java.io.Serializable;
 
 /**
  * A class containing information on an employee.
@@ -7,12 +8,22 @@ package storage.users;
  * @author Cullen Sturdivant
  * @version 1.0, 10/2/2017
  */
-public class Employee extends User {
+public class Employee extends User implements Serializable {
     
-    private int ssn;                // Social Security Number
-    private float monthlySalary;
-    private int ban;                // Bank Account Number
+    private final int ssn;                // Social Security Number
+    private final float monthlySalary;
+    private final int ban;                // Bank Account Number
     
+    /**
+     * Constructor for an Employee.
+     * 
+     * @param userID the unique ID of the employee.
+     * @param firstName the first name of the employee.
+     * @param lastName the last name of the employee.
+     * @param ssn the social security number (SSN) of the employee.
+     * @param monthlySalary the monthly salary of the employee.
+     * @param ban the direct deposit bank account number of the employee.
+     */
     public Employee(int userID, String firstName, String lastName,
                     int ssn, float monthlySalary, int ban) {
         super(userID, firstName, lastName);
@@ -21,14 +32,29 @@ public class Employee extends User {
         this.ban = ban;
     }
     
+    /**
+     * Accessor for the social security number (SSN).
+     * 
+     * @return the social security number of the employee.
+     */
     public int getSSN() {
         return this.ssn;
     }
     
+    /**
+     * Accessor for the salary of the employee.
+     * 
+     * @return the salary the employee makes every month.
+     */
     public float getMonthlySalary() {
         return this.monthlySalary;
     }
     
+    /**
+     * Accessor for the employee's direct deposit bank account number.
+     * 
+     * @return the bank account number of the employee.
+     */
     public int getBankAccountNumber() {
         return this.ban;
     }
--- ShippingCenter/storage/users/User.java
+++ ShippingCenter/storage/users/User.java
@@ -1,5 +1,6 @@
 package storage.users;
 
+import java.io.Serializable;
 
 /**
  * The base class of all users in the system.
@@ -7,30 +8,57 @@ package storage.users;
  * @author Cullen Sturdivant
  * @version 1.0, 10/2/2017
  */
-public class User {
+public class User implements Serializable {
 
     private final int userID;
     private final String firstName;
     private final String lastName;
     
+    /**
+     * Constructor for a user.
+     * 
+     * @param userID the unique ID of the user.
+     * @param firstName the first name of the user.
+     * @param lastName the last name of the user.
+     */
     public User(int userID, String firstName, String lastName) {
         this.userID = userID;
         this.firstName = firstName;
         this.lastName = lastName;
     }
     
+    /**
+     * Accessor for the userID.
+     * 
+     * @return the ID of the user.
+     */
     public int getUserID() {
         return this.userID;
     }
     
+    /**
+     * Accessor for the first name of the user.
+     * 
+     * @return the first name of the user.
+     */
     public String getFirstName() {
         return this.firstName;
     }
     
+    /**
+     * Accessor for the last name of the user.
+     * 
+     * @return the last name of the user.
+     */
     public String getLastName() {
         return this.firstName;
     }
     
+    /**
+     * Combines the first and last names of a user into one String.
+     * 
+     * @return the full name of the user concatenated.
+     */
     public String getFullName() {
         String fullName;
         fullName = this.firstName + " " + this.lastName;
